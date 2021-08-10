b5ad5c6b57772e70530598a24079290de6d4fc4d
Johannes <Johannesolsson8@gmail.com>
2021-08-10 18:32:20 +0200

    Adding framer

--- src/components/images/Images.tsx
+++ src/components/images/Images.tsx
@@ -1,6 +1,7 @@
 import { useContext, useEffect } from "react";
 import { Background } from "../../utils/BackgroundContext";
 import { ReactComponent as ActiveImageIcon } from "../../icons/check.svg";
+import { motion } from "framer-motion";
 
 export interface IImages {
   images: IImage[];
@@ -12,21 +13,46 @@ export interface IImage {
   alt: string;
 }
 
+const container = {
+  visible: {
+    opacity: 1,
+    scale: 1,
+    transition: {
+      delayChildren: 0.3,
+      staggerChildren: 0.2,
+    },
+  },
+};
+
+const item = {
+  hidden: { y: 15, opacity: 0 },
+  visible: {
+    y: 0,
+    opacity: 1,
+  },
+};
+
 export const Images = (props: IImages) => {
   const context = useContext(Background);
   const { returnValue, setActiveImage } = context;
 
   useEffect(() => {
-    setActiveImage(props.images[0].srcFull);
+    setActiveImage(props.images[0].srcFull, props.images[0].alt);
     // No need to ever run this useEffect more after render
     // eslint-disable-next-line
   }, []);
 
   return (
     <>
-      <div className="flex absolute bottom-36 w-full px-4 justify-between sm:px-8 md:justify-start lg:justify-end lg:static lg:w-auto lg:px-0 lg:mx-20 xl:mx-44">
+      <motion.div
+        variants={container}
+        initial="hidden"
+        animate="visible"
+        className="flex absolute bottom-36 w-full px-4 justify-between sm:px-8 md:justify-start lg:justify-end lg:static lg:w-auto lg:px-0 lg:mx-20 xl:mx-44"
+      >
         {props.images.map((x: IImage) => (
-          <button
+          <motion.button
+            variants={item}
             aria-label={`Click to enlarge this image of ${x.alt}`}
             aria-pressed={returnValue === x.srcFull}
             key={x.alt}
@@ -34,7 +60,7 @@ export const Images = (props: IImages) => {
               backgroundImage: `url(${x.srcPrev})`,
               backgroundSize: "cover",
             }}
-            onClick={() => setActiveImage(x.srcFull)}
+            onClick={() => setActiveImage(x.srcFull, x.alt)}
             className={`flex h-24 w-1/4 mt-4 border-2 rounded shadow-md sm:w-32 md:mr-2 lg:mr-0 lg:mx-0 lg:ml-2 focus:border-indigo-400 focus:outline-none ${
               returnValue === x.srcFull ? "border-indigo-300" : ""
             }`}
@@ -47,9 +73,9 @@ export const Images = (props: IImages) => {
                 <ActiveImageIcon className="w-3 h-3 text-indigo-600" />
               </div>
             )}
-          </button>
+          </motion.button>
         ))}
-      </div>
+      </motion.div>
     </>
   );
 };
