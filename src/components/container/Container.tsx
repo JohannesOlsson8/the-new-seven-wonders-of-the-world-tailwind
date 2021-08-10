01b59af05246d45aa627387211a2f410d0c3637e
Johannes <Johannesolsson8@gmail.com>
2021-08-10 18:33:43 +0200

    Container

--- src/components/container/Container.tsx
+++ src/components/container/Container.tsx
@@ -3,7 +3,9 @@ import classes from "./Container.module.scss";
 export const Container: React.FC = (props) => {
   return (
     <>
-      <div className={`h-screen flex flex-col bg-cover ${classes.gradient}`}>
+      <div
+        className={`h-screen flex flex-col bg-cover absolute z-20 w-full ${classes.gradient}`}
+      >
         {props.children}
       </div>
     </>
